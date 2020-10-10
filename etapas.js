a10d093da10e62199a82cb99b4b1170a8c653dd6
unknown <guerreiramaya2016@gmail.com>
2020-10-10 04:19:55 -0300

    Adicionado arquivos...

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo:'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero:'45123',
+                nome: 'Chico Rufino',
+                partido: 'PSDB',
+                fotos: [
+                    {url:'homem5.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero:'77777',
+                nome: 'Dedé Florêncio',
+                partido: 'PP',
+                fotos: [
+                    {url:'homem6.png', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros:2,
+        candidatos: [
+            {
+                numero:'45',
+                nome: 'Coloral',
+                partido: 'PSDB',
+                vice: 'Dr. Alessandro',
+                fotos : [
+                    {url:'homem1.png', legenda: 'Prefeito'},
+                    {url: 'homem2.png', legenda: 'Vice-Prefeito', small:true}
+                ]
+            },
+            {
+                numero:'40',
+                nome: 'Celinho',
+                partido: 'PSB',
+                vice: 'Mazinho',
+                fotos : [
+                    {url:'homem3.png', legenda: 'Prefeito'},
+                    {url: 'homem4.png', legenda: 'Vice-Prefeito', small:true}
+                ]
+            },
+        ]
+    }
+];
